e79b5d29834de5b1e349707f267292bea729bd57
mubarok <dikur@dikurs-MacBook-Pro.local>
2024-12-15 23:21:46 +0700

    Progress the development process

---
+++ WeatherTracker/WeatherTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerApp.swift
+//  WeatherTracker
+//
+//  Created by mubarok on 15/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherTrackerTests/WeatherTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherTrackerTests.swift
+//  WeatherTrackerTests
+//
+//  Created by mubarok on 15/12/24.
+//
+
+import Testing
+
+struct WeatherTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
